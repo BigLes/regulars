1a852474816f896294a2f95798a0f80233843121
BigLes <bigles@bigmir.net>
2016-11-17 21:08:57 +0100

    add puzzle component

---
+++ frontend/components/puzzle/Puzzle.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Oleksandr Lisovyk on 17.11.2016.
+ */
+'use strict';
+
+import React        from 'react';
+import style        from './style'
+import {css}        from 'aphrodite';
+import classNames   from 'classnames';
+import shortid      from 'shortid';
+
+class Puzzle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (<div></div>);
+    }
+}
+
+export default Puzzle;
---
+++ frontend/components/puzzle/style.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Oleksandr Lisovyk on 17.11.2016.
+ */
+'use strict';
+
+import {StyleSheet} from 'aphrodite';
+import constants    from '../../constants/styles';
+
+export default StyleSheet.create({
+    puzzle: {}
+});
