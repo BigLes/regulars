3fc58ea3ae472602f596082f2194eec8b29c494d
Oleksandr <BigLes@users.noreply.github.com>
2016-11-23 18:41:07 +0100

    update object.assign

--- frontend/components/userButton/UserButton.js
+++ frontend/components/userButton/UserButton.js
@@ -39,7 +39,7 @@ class UserButton extends React.Component {
     }
 
     __onUserStoreChange() {
-        this.setState(Object.assign(this.state, {currentUser: UserStore.getCurrentUser()}));
+        this.setState(Object.assign({}, this.state, {currentUser: UserStore.getCurrentUser()}));
     }
 
     render() {
@@ -63,7 +63,7 @@ class UserButton extends React.Component {
     }
 
     __toggle() {
-        this.setState(Object.assign(this.state, {active: !this.state.active}));
+        this.setState(Object.assign({}, this.state, {active: !this.state.active}));
     }
 
     activateUser() {
