605bdf75038714abc3f7f12a016bc28ea12400b7
BigLes <bigles@bigmir.net>
2017-07-15 19:13:23 +0200

    temporary turn off mailer functionality

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -7,28 +7,29 @@ const nodemailer    = require('nodemailer');
 const fs            = require('fs');
 const config        = require('config');
 
-const transporter = nodemailer.createTransport(JSON.parse(fs.readFileSync('backend/utils/connectionString.txt').toString()));
+// const transporter = nodemailer.createTransport(JSON.parse(fs.readFileSync('backend/utils/connectionString.txt').toString()));
 
 module.exports = {
-    sendInvitationEmail: (user) => {
+    sendInvitationEmail(user) {
         //TODO: provide good options and templates
-        const mailOptions = {
-            from: '"Regular Expressions Puzzle" <regular.expressions.puzzle@gmail.com>',
-            to: user.email,
-            subject: 'Welcome',
-            //TODO: add functionality to re-send email
-            text: `Hello, ${user.login}! To verify your email, please use this link: ${config.server.url + '/verify?token=' + user.token}`,
-            html: `<b>Hello, ${user.login}!</b><br /><p>To verify your email, please use this link: <a href="${config.server.url + '/verify?token=' + user.token}">activation</a></p>`
-        };
-
-        return new Promise((resolve, reject) => {
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.log(error);
-                    return reject(error);
-                }
-                return resolve('Message sent: ' + info.response);
-            });
-        });
+        // const mailOptions = {
+        //     from: '"Regular Expressions Puzzle" <regular.expressions.puzzle@gmail.com>',
+        //     to: user.email,
+        //     subject: 'Welcome',
+        //     //TODO: add functionality to re-send email
+        //     text: `Hello, ${user.login}! To verify your email, please use this link: ${config.server.url + '/verify?token=' + user.token}`,
+        //     html: `<b>Hello, ${user.login}!</b><br /><p>To verify your email, please use this link: <a href="${config.server.url + '/verify?token=' + user.token}">activation</a></p>`
+        // };
+        //
+        // return new Promise((resolve, reject) => {
+        //     transporter.sendMail(mailOptions, (error, info) => {
+        //         if (error) {
+        //             console.log(error);
+        //             return reject(error);
+        //         }
+        //         return resolve('Message sent: ' + info.response);
+        //     });
+        // });
+        return Promise.resolve();
     }
 };
